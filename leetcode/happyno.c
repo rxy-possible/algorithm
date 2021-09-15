d0a8bf4ab93dedc2ffaeaa31584329906b02bec0
rxy-possible <rxy18645231697@163.com>
2021-09-16 00:40:00 +0800

    happy number

---
+++ leetcode/happyno.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#define bool char
+#define true 1
+#define false 0
+
+int next_n(int n)       //每位平方和的下一位数
+{
+	int r = 0;
+	while (n != 0)
+	{
+		int d = n % 10;
+		n /= 10;
+		r += d * d;
+	}
+	return r;
+}
+
+bool contains(int* history, int size, int n) //判断这个数字是否曾经出现过
+{
+	for (int i = 0; i < size; i++)
+	{
+		if (history[i] == n)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+bool isHappy(int n)                //最终的结果
+{
+	
+
+	int history[10000];
+	int size = 0;
+
+	while (!contains(history, size, n))
+	{
+		printf("%d\n", next_n(n));
+		history[size] = n;
+		size++;
+		n = next_n(n);
+	}
+	
+
+	return false;
+}
+
+int main()
+{
+	bool a = isHappy(20);
+	return 0;
+}
